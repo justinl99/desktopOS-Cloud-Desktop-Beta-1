67062d08727988de8736298fef42eef22082a7b9
JustinL <93564256+hockerdiscord@users.noreply.github.com>
2021-12-31 09:06:07 +0700

    Update apps.store.ts

--- src/stores/apps.store.ts
+++ src/stores/apps.store.ts
@@ -16,8 +16,8 @@ export const openAppsStore = atom<Record<AppID, boolean>>({
   calendar: false,
   'system-preferences': false,
 
-  'purus-twitter': true,
-  'view-source': true,
+  'purus-twitter': false,
+  'view-source': false,
 });
 
 /** Which app is currently focused */
